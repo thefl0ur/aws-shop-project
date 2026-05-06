2aaf75cbbec63f297cf4766176b5b508369d5b31
Калинкин Павел <pnzazfof@gmail.com>
2026-05-06 23:38:58 +0200

    script for db seed

--- backend/product_service/src/shared/model.py
+++ backend/product_service/src/shared/model.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel, Field
 
 class ProductModel(BaseModel):
     id: str
-    title: str = ""
-    description: str = ""
+    title: str
+    description: str | None = None
     price: Decimal = Field(gt=Decimal("0.0"))
+
+
+class StockModel(BaseModel):
+    product_id: str
+    count: int = Field(ge=0)
