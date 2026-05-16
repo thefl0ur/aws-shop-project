170ab91435e8996e0df37be1f3b6a6877c9d3714
Калинкин Павел <pnzazfof@gmail.com>
2026-05-16 12:09:29 +0200

    tests for import servoce

--- backend/import_service/services/import_products_file/main.py
+++ backend/import_service/services/import_products_file/main.py
@@ -18,17 +18,12 @@ router = Router()
 s3_client = boto3.client("s3")
 
 
-@router.get("/import")
-def import_products_file():
-    file_name = router.current_event.get_query_string_value("name")
-    if not file_name:
-        raise BadRequestError("Missing 'name' query parameter")
-
+def generate_presigned_url(s3, bucket_name: str, file_name: str) -> str:
     try:
-        signed_url = s3_client.generate_presigned_url(
+        return s3.generate_presigned_url(
             "put_object",
             Params={
-                "Bucket": os.environ["BUCKET_NAME"],
+                "Bucket": bucket_name,
                 "Key": f"uploaded/{file_name}",
                 "ContentType": "text/csv",
             },
@@ -36,6 +31,15 @@ def import_products_file():
         )
     except Exception:
         raise InternalServerError("Failed to prepare signed url")
+
+
+@router.get("/import")
+def import_products_file():
+    file_name = router.current_event.get_query_string_value("name")
+    if not file_name:
+        raise BadRequestError("Missing 'name' query parameter")
+
+    signed_url = generate_presigned_url(s3_client, os.environ["BUCKET_NAME"], file_name)
 
     return Response(status_code=200, content_type="text/plain", body=signed_url)
 
