037a2389841d4f4784a15ed9caae1cd7d7b48e21
Калинкин Павел <pnzazfof@gmail.com>
2026-05-27 18:15:19 +0200

    auth lambda

--- backend/authorization_service/infra/stack/auth_stack.py
+++ backend/authorization_service/infra/stack/auth_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_iam as iam,
+    CfnOutput,
 )
 from constructs import Construct
 
@@ -10,10 +11,24 @@ class AuthorizationServiceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        user = self.node.try_get_context("test_user")
+        password = self.node.try_get_context("test_password")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AuthorizationServiceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.authorizer_lambda = _lambda.Function(
+            self,
+            "basicAuthorizer",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="main.handler",
+            code=_lambda.Code.from_asset("services/basic_auth"),
+            environment={user: password},
+        )
+        self.authorizer_lambda.add_permission(
+            "AllowApiGatewayInvoke",
+            principal=iam.ServicePrincipal("apigateway.amazonaws.com"),
+        )
+        CfnOutput(
+            self,
+            "BasicAuthorizerArnExport",
+            value=self.authorizer_lambda.function_arn,
+            export_name="BasicAuthorizerArn",
+        )
